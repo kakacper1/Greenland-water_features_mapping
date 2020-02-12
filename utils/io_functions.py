b5ed96cec0820106563d1ed99bc053be8ba2166a
kakacper1 <kakacper1@gmail.com>
2020-02-12 01:09:57 +0100

    initial commit

---
+++ utils/io_functions.py
@@ -0,0 +1,64 @@
+import os
+import geopandas as gpd
+from sentinelhub import CRS, BBox
+
+
+
+def get_year(date_string):
+    """Date input format: '2017-03-01' YYYY-MM-DD """
+    return  str(date_string[:4])
+
+def get_month(date_string):
+    """Date input format: '2017-03-01' YYYY-MM-DD """
+    return str(date_string[5:7])
+
+def get_day(date_string):
+    """Date input format: '2017-03-01' YYYY-MM-DD """
+    return str(date_string[8:10])
+
+def load_all_utm_zone_tiles(utm_zone, tile_size_in_km):
+    """ This function loads all filed from a certain ymt zone """
+    data_dir = '../../data/utm_tile_division/{1}/all_tiles_{0:.0f}x{0:.0f}km.shp'.format(tile_size_in_km, utm_zone.name )
+    return gpd.read_file(data_dir)
+
+def get_selected_tiles(gdf, top_l_x, top_l_y, bot_r_x, bot_r_y, min_idx = 0, max_idx = 100000 ):
+    """ This function returns list of indexes of tiles selected by a rectangle from provided tiles """
+    print('Original number of tiles:',len(gdf))
+    
+    selected_tiles_idxs_tmp =  gdf[gdf['index_y'].between(bot_r_y, top_l_y, inclusive=True) ]
+    print('After filtering all different Y indexes:',len(selected_tiles_idxs_tmp))
+    selected_tiles_idxs_second_tmp =  selected_tiles_idxs_tmp[selected_tiles_idxs_tmp['index_x'].between(top_l_x, bot_r_x, inclusive=True)]
+    print('After filtering all different X indexes:',len(selected_tiles_idxs_second_tmp))
+    selected_tiles_idxs_gdf =  selected_tiles_idxs_second_tmp[selected_tiles_idxs_second_tmp['index'].between(min_idx, max_idx, inclusive=True)]
+    print('After filtering all different utm indexes indexes:',len(selected_tiles_idxs_gdf))
+    return selected_tiles_idxs_gdf
+
+def save_selected_tiles_to_file(selected_tiles_gdf, tile_size_in_km, site_name, crs_zone ):
+    
+    selected_tiles_gdf.to_crs(crs={'init': CRS.ogc_string(crs_zone)}) # world.to_crs(epsg=3395) would also work
+    directory_path = '../../data/selected_tiles/'+site_name+'/'+crs_zone.name
+    if not os.path.isdir(directory_path):
+        os.makedirs(directory_path)
+    
+    shapefile_filepath = directory_path+'/selected_tiles_{0:.0f}x{0:.0f}km.shp'.format(tile_size_in_km)
+    selected_tiles_gdf.to_file(shapefile_filepath)
+    print('File succesfully saved to:', shapefile_filepath ) 
+    return
+
+def load_selected_tiles_from_file(site_name, crs_zone, tile_size_in_km=10 ):
+    """To produce a string and loads selected patches determined by site name tile size and crs zone if saved before"""
+    directory_path = '../../data/selected_tiles/'+site_name+'/'+crs_zone.name
+    shapefile_filepath = directory_path+'/selected_tiles_{0:.0f}x{0:.0f}km.shp'.format(tile_size_in_km)
+
+    if os.path.isfile(shapefile_filepath):
+        print ("File exists")
+    else:
+        print ("Requested file does not exist")
+        print(shapefile_filepath)
+        return 
+    return gpd.read_file(shapefile_filepath)
+
+def get_eopatches_dir(data_product, site_name, crs, date_range ):
+    date_range_string = '{0}_{1}-{2}_{3}'.format(get_year(date_range[0]),get_month(date_range[0]),get_year(date_range[1]),get_month(date_range[1]))
+    filepath = '../../data/EOPatches/{0}/{1}/{2}/{3}/'.format(data_product,site_name,crs.name, date_range_string)
+    return filepath
---
+++ utils/plot_functions.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+from sentinelhub import CRS, BBox
+import os
+import numpy as np
+import geopandas as gpd
+
+def display_greenland_with_tiles(gdf, crs_zone, display_indexes=False):
+    """Display greeenland borders with selected tiles with indexes"""
+    
+    # Folder where Greenland outline data is stored:
+    DATA_FOLDER = os.path.join('..', '..', 'data', 'geoJSONs')
+    # import geojson of Greenland:
+    grl_geo_json = gpd.read_file(os.path.join(DATA_FOLDER, 'GRL.geojson'))
+    print('Succesfully loaded Greenland contour. Current CRS:', grl_geo_json.crs)
+    # change CRS
+    grl_geo_json_UTM_zone = grl_geo_json.to_crs(crs={'init': CRS.ogc_string(crs_zone)})
+    # Check what crs is obtained outline now:
+    print('CRS chnaged:', grl_geo_json_UTM_zone.crs)
+    
+    # DISPLAY RESULTS:
+    # set up font
+    fontdict = {'family': 'monospace', 'weight': 'normal', 'size': 8}
+    if display_indexes:
+        # if bboxes have all same size, estimate offset
+        xl, yl, xu, yu = gdf.iloc[0].geometry.bounds
+        xoff, yoff = (xu-xl)/3, (yu-yl)/5
+    
+    #figure 
+    fig, ax = plt.subplots(figsize=(200, 360))
+    gdf.plot(ax=ax,facecolor='w',edgecolor='r',alpha=0.5)
+    grl_geo_json_UTM_zone.plot(ax=ax, facecolor='w',edgecolor='b',alpha=0.5, linewidth=4)
+    plt.title(crs_zone.name + 'zone of Greenland')
+    plt.axis('on')
+    if display_indexes:
+        for idx in gdf.index:
+            eop_name = '{0}x{1}'.format(gdf.index_x[idx], gdf.index_y[idx])
+            centroid, = list(gdf.geometry[idx].centroid.coords)
+            ax.text(centroid[0]-xoff, centroid[1]+yoff, '{}'.format(idx), fontdict=fontdict)
+            ax.text(centroid[0]-xoff, centroid[1]-yoff, eop_name, fontdict=fontdict)
+    
+    return
+
+
+def plot_RGB_LANDSAT_8_image(eopatch, data_acces_name='LANDSAT_RAW_BANDS' ,datetime_idx=0, red_id=4, green_id=3, blue_id=2  ):
+    
+    fig = plt.figure(figsize=(20, 20))
+
+    print(eopatch.timestamp[datetime_idx])
+    plt.imshow(np.clip(eopatch.data[data_acces_name][datetime_idx][..., [red_id, green_id, blue_id]] * 3.5, 0, 1))
+    plt.xticks([])
+    plt.yticks([])
+    del eopatch
+    return
+
+def plot_single_band_LANDSAT_8(eopatch, band_idx, data_acces_name='LANDSAT_RAW_BANDS' ,datetime_idx =0 ):
+    fig = plt.figure(figsize=(10, 10)) 
+    print(eopatch.timestamp[datetime_idx])
+    plt.imshow(eopatch.data[data_acces_name][datetime_idx][..., band_idx].squeeze())
+    plt.xticks([])
+    plt.yticks([])
+    del eopatch
+    return
+
+def plot_qa_mask_LANDSAT_8(eopatch, band_idx, mask_acces_name='LANDSAT_QA_LAYERS', datetime_idx =0 ):
+    fig = plt.figure(figsize=(10, 10)) 
+    print(eopatch.timestamp[datetime_idx])
+    plt.imshow(eopatch.data[mask_acces_name][datetime_idx][..., band_idx].squeeze())
+    plt.xticks([])
+    plt.yticks([])
+    del eopatch
+    return
+
+
+def plot_timeless_mask_LANDSAT_8(eopatch, band_idx, mask_acces_name='DEM_RAW_LAYER' ):
+    fig = plt.figure(figsize=(10, 10)) 
+    plt.imshow(eopatch.data_timeless[mask_acces_name][..., band_idx].squeeze())
+    plt.xticks([])
+    plt.yticks([])
+    del eopatch
+    return
+
